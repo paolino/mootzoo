19285c40dd141f75174ce5bc9b291c058aa0bb86
Paolo Veronelli <paolo.veronelli@gmail.com>
2015-05-27 23:51:23 +0200

    json

--- mootzoo.js
+++ mootzoo.js
@@ -1,8 +1,25 @@
 {
 var app = angular.module('mootzoo', []);
+app.author=false;
+app.logged = false;
+app.unused=false;
+app.controller('conversations', function($scope, $http) {
+    $http.get("conversations.json").success(function(response) {$scope.conversations = response;});
 
-};
 
+    $scope.convButton= function(x){
+     if(x.type=="complete")
+            return "btn btn-primary"
+     else if(x.type=="orphan")
+            return "btn btn-danger"
+     else
+            return "btn btn-default"
+     };
+
+
+});
+}
+    
 window.onload = function() {
     var t = document.getElementsByTagName('textarea')[0];
     var offset= !window.opera ? (t.offsetHeight - t.clientHeight) : (t.offsetHeight + parseInt(window.getComputedStyle(t, null).getPropertyValue('border-top-width'))) ;
@@ -20,4 +37,3 @@ window.onload = function() {
         resize(t);
     });
 }
-
